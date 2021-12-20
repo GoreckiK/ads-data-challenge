4bc72c38df0b8a74accb5c92009033574dbcabee
Krzysztof Gorecki <krzysztof.gorecki@securitas.com>
2021-12-20 09:45:00 +0100

    csv to json converter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useConvertCSVtoJSON } from "./hooks/common";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	const { convertedData } = useConvertCSVtoJSON("http://adverity-challenge.s3-website-eu-west-1.amazonaws.com/DAMKBAoDBwoDBAkOBAYFCw.csv");
+	return <div className="App"></div>;
 }
 
 export default App;
